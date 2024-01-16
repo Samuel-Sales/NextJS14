7aa866df6fd4d7e53883a4e48c44f29314bd352d
Samuel-Sales <cacabrbf@hotmail.com>
2024-01-16 05:47:21 -0300

    Private Folders

---
+++ hello-world/src/app/_lib/page.jsx
@@ -0,0 +1,8 @@
+// For separating UI logic from routing logic
+// For consistently organizing internal files across a project
+// For sorting and grouping files in code editors
+// And finally, for avoiding potential naming conflicts with future Next.js file conventions
+
+export default function PrivateRoute() {
+    return <h1>You can't view this in the browser</h1>
+}
